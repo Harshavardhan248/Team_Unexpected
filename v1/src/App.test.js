2a122c0a52729c0a14dfd4bc3fb8a317feaf916b
Harsha <hv9876.9876@gmail.com>
2024-12-08 22:23:32 -0800

    Delete v1 directory

--- v1/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders HR Portal link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/HR Portal/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- v1/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
-  withCredentials: true, // Include cookies with each request
-});
-
-export default api;
